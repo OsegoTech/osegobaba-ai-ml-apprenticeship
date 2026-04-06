01b03f518b22575240413da87dfaf3b8498d07b4
Omollo Cliff Oganda <49682320+OsegoTech@users.noreply.github.com>
2026-04-06 09:00:08 +0300

    Assignment starter: assignment/week-1-day-2-slug

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# Simple calculator
+
+# TODO: Prompt user for the first number
+# TODO: Prompt user for the second number
+# TODO: Prompt user for the operator (+, -, *, /)
+# TODO: Perform the calculation based on user input
+# TODO: Print the result
