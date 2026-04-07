5cf90c984985b8d542eecbe624fae991f6a8f4ef
Omollo Cliff Oganda <49682320+OsegoTech@users.noreply.github.com>
2026-04-07 22:02:08 +0300

    Fix typo in goodbye message

--- calculator.py
+++ calculator.py
@@ -71,7 +71,7 @@ def main():
         else:
             print(f"\n  ➜  {format_result(a)} {op} {format_result(b)} = {format_result(result)}\n")
 
-    print("\nGooduubye! 👋")
+    print("\nGooduuuuuuubye! 👋")
 
 
 if __name__ == "__main__":
